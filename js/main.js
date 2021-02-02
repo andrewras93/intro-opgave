f1c567d3845b43f8964cff2533d43fd51c1b2697
Andrew <andrewras93@gmail.com>
2021-02-02 14:22:32 +0100

    Added alert box on contact page when clicking submit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Toggle Mobile Menu
+
 function toggleClass(){
 
     let menu = document.querySelector('.mainMenu');
@@ -9,3 +11,20 @@ function toggleClass(){
 let hamburger = document.querySelector('#hamburger');
 
 hamburger.addEventListener('click', toggleClass);
+
+// Contact Form
+
+function getFormData(){
+
+    let name = document.querySelector('#name').value;
+    let email = document.querySelector('#email').value;
+    let besked = document.querySelector('#besked').value;
+
+    alert('Hej ' + name + ' tak for din besked, hvor du skrev: ' + besked +
+    '. Vi vender tilbage til dig på din email ' + email + ' snarest muligt.');
+
+}
+
+let submit = document.querySelector('#submitMsg');
+
+submit.addEventListener('click', getFormData);
